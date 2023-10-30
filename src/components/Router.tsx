0f07fbecabbda5deee4bdc381849840d1558ff9b
SeungGukYoo <dbtmd878@gmail.com>
2023-10-30 15:09:29 +0900

    feat: 라우트에 경로에 알맞는 컴포넌트 연결

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from '../pages/home';
+import PostList from '../pages/posts';
+import PostDetail from '../pages/posts/detail';
+import PostEdit from '../pages/posts/edit';
+import PostNew from '../pages/posts/new';
+import Profile from '../pages/profile';
+import SignInPage from '../pages/signin';
+import SignUpPage from '../pages/signup';
 
 function Router() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>hello world</h1>} />
-        <Route path="/posts" element={<h1>hello world</h1>} />
-        <Route path="/posts/:id" element={<h1>hello detail</h1>} />
-        <Route path="/posts/new" element={<h1>hello new page</h1>} />
-        <Route path="/posts/edit/:id" element={<h1>hello edit page</h1>} />
-        <Route path="/profile" element={<h1>profile page</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts/new" element={<PostNew />} />
+        <Route path="/posts/edit/:id" element={<PostEdit />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/profile" element={<SignUpPage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </>

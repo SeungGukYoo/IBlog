9b712351d9de02db35df911e7260a0fc1f4be1c5
SeungGukYoo <dbtmd878@gmail.com>
2023-11-01 15:23:12 +0900

    feat: 파이어베이스 초기화 설정

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,5 +1,6 @@
-import { initializeApp } from 'firebase/app';
+import { FirebaseApp, getApp, initializeApp } from 'firebase/app';
 
+let app: FirebaseApp;
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -9,6 +10,10 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const firebase = initializeApp(firebaseConfig);
+try {
+  app = getApp('app');
+} catch (el) {
+  app = initializeApp(firebaseConfig, 'app');
+}
 
-export default firebase;
+export default app;

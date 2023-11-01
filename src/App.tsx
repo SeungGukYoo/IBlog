8ca9cf2a50ce2f526ead5a7c35806db6a5d4941d
SeungGukYoo <dbtmd878@gmail.com>
2023-11-01 16:56:50 +0900

    feat: 사용자 관찰 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import Loader from 'components/Loader';
 import Router from 'components/Router';
-import { getAuth } from 'firebase/auth';
+import { Auth, getAuth, onAuthStateChanged } from 'firebase/auth';
 import app from 'firebaseApp';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!getAuth(app)?.currentUser);
+  const [init, setInit] = useState(false);
 
+  useEffect(() => {
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
+
       <ToastContainer />
     </>
   );

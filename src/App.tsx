06786f92612e05cca0c9fd3fe83caa301a102078
SeungGukYoo <dbtmd878@gmail.com>
2023-11-01 16:15:09 +0900

    feat: react-toastify css 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { getAuth } from 'firebase/auth';
 import app from 'firebaseApp';
 import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!getAuth(app)?.currentUser);

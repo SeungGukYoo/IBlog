1ea883f26a81ae637a2da7e4ce578a68f291449b
SeungGukYoo <dbtmd878@gmail.com>
2023-11-01 15:23:54 +0900

    feat: Router 컴포넌트에 인증값 전달

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Router from 'components/Router';
-import React from 'react';
+import { getAuth } from 'firebase/auth';
+import app from 'firebaseApp';
 
+import React, { useEffect, useState } from 'react';
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!getAuth(app)?.currentUser);
+
   return (
     <>
-      <Router />
+      <Router isAuthenticated={isAuthenticated} />
     </>
   );
 }

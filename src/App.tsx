1d9bcc91b8250a1052ca04029718f92ae698c691
SeungGukYoo <dbtmd878@gmail.com>
2023-11-01 16:05:53 +0900

    feat: react-toasify 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Router from 'components/Router';
 import { getAuth } from 'firebase/auth';
 import app from 'firebaseApp';
+import React, { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
-import React, { useEffect, useState } from 'react';
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!getAuth(app)?.currentUser);
 
   return (
     <>
       <Router isAuthenticated={isAuthenticated} />
+      <ToastContainer />
     </>
   );
 }

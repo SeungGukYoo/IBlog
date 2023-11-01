91391af9dd90a8f1db16fa8bf88bd2128da28a48
SeungGukYoo <dbtmd878@gmail.com>
2023-11-01 14:08:27 +0900

    feat: 경로에 따른 라우트 분리

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -6,22 +6,31 @@ import PostNew from 'pages/posts/new';
 import ProfilePage from 'pages/profile';
 import SignInPage from 'pages/signin';
 import SignUpPage from 'pages/signup';
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 function Router() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/posts/new" element={<PostNew />} />
-        <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="*" element={<Navigate replace to="/" />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/posts/new" element={<PostNew />} />
+            <Route path="/posts/edit/:id" element={<PostEdit />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </>
+        ) : (
+          <>
+            <Route path="*" element={<SignInPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </>
+        )}
       </Routes>
     </>
   );

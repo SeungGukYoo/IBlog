ed35a15658accb2abcc30e40253dfcccda98a084
SeungGukYoo <dbtmd878@gmail.com>
2023-11-02 13:55:32 +0900

    feat: auth Context 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { AuthContextProvider } from 'context/AuthContext';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,9 +7,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </AuthContextProvider>,
 );

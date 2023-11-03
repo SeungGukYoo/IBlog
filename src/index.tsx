9427ba212016c41882680e822a4c7ac1f7fbc61d
SeungGukYoo <dbtmd878@gmail.com>
2023-11-03 16:52:42 +0900

    feat: post context 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { AuthContextProvider } from 'context/AuthContext';
+import { PostsProviderContext } from 'context/PostsContext';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -8,8 +9,10 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <AuthContextProvider>
-    <Router>
-      <App />
-    </Router>
+    <PostsProviderContext>
+      <Router>
+        <App />
+      </Router>
+    </PostsProviderContext>
   </AuthContextProvider>,
 );

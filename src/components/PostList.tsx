7a310d6cceb2ba48c8cfa622d58efc573f01d7ef
SeungGukYoo <dbtmd878@gmail.com>
2023-11-03 16:56:26 +0900

    feat:  context 값으로 변경

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,7 +1,6 @@
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from 'firebaseApp';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 
+import PostsContext from 'context/PostsContext';
 import Post from './Post';
 
 interface Props {
@@ -15,29 +14,11 @@ export interface PostType {
   user: string;
   id: string;
 }
-type PostsType = Array<PostType>;
+
 type TabType = 'all' | 'my';
 function PostList({ hasNavigation = true }: Props) {
   const [activeTab, setActiveTab] = useState<TabType>('all');
-  const [posts, setPosts] = useState<PostsType>([]);
-  useEffect(() => {
-    const getPosts = async () => {
-      const querySnapShot = await getDocs(collection(db, 'posts'));
-      const arr: PostsType = [];
-      querySnapShot.forEach(doc => {
-        const obj = {
-          id: doc.id,
-          ...doc.data(),
-        } as PostType;
-        arr.push(obj);
-      });
-      setPosts(prev => {
-        return [...prev, ...arr];
-      });
-    };
-
-    getPosts();
-  }, []);
+  const { posts } = useContext(PostsContext);
 
   return (
     <>
@@ -61,8 +42,8 @@ function PostList({ hasNavigation = true }: Props) {
       )}
 
       <div className="post__list">
-        {posts.length > 0 ? (
-          posts.map(post => <Post post={post} key={post.id} />)
+        {posts && posts.length > 0 ? (
+          posts?.map(post => <Post post={post} key={post.id} />)
         ) : (
           <div className="post__no-post">게시글이 없습니다.</div>
         )}

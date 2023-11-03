63df48b9a3b36b3932a79f8003894d6118f0781f
SeungGukYoo <dbtmd878@gmail.com>
2023-11-03 16:55:32 +0900

    feat: 커스텀 훅으로 변경

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,68 +1,44 @@
-import AuthContext from 'context/AuthContext';
-import { addDoc, collection } from 'firebase/firestore';
-import { db } from 'firebaseApp';
-import React, { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import PostsContext from 'context/PostsContext';
+import useForm from 'hooks/useForm';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import type { PostType } from './PostList';
 
 function PostForm() {
-  const { user } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [summary, setSummary] = useState('');
-  const [content, setContent] = useState('');
+  const { firebaseClient } = useContext(PostsContext);
+  const { id } = useParams();
+  const { title, summary, content, onChange, setPost, onSubmit } = useForm();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    e.preventDefault();
-
-    const { name, value } = e.target;
-
-    if (name === 'title') {
-      setTitle(value);
+  useEffect(() => {
+    if (id) {
+      const getData = async () => {
+        const data = await firebaseClient?.getPosts(id);
+        setPost({ ...(data as PostType) });
+      };
+      getData();
     }
-    if (name === 'summary') {
-      setSummary(value);
-    }
-    if (name === 'content') {
-      setContent(value);
-    }
-  };
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    try {
-      e.preventDefault();
-      const docRef = await addDoc(collection(db, 'posts'), {
-        user: user?.email,
-        title,
-        summary,
-        content,
-        createdAt: new Date()?.toLocaleDateString(),
-      });
-      console.log(docRef);
-      toast.success('게시글이 성공적으로 저장되었습니다 👏', {
-        autoClose: 1000,
-        pauseOnHover: false,
-      });
-      navigate('/');
-    } catch (error) {
-      console.error(error);
-      toast.error('예기치 않은 오류가 발생하였습니다❗️', { autoClose: 1000, pauseOnHover: false });
-    }
-  };
+  }, [firebaseClient, id, setPost]);
 
   return (
     <form onSubmit={onSubmit} action="/post" method="POST" className="form">
       <div className="form__block">
         <label htmlFor="title">제목</label>
-        <input onChange={onChange} type="text" name="title" required id="title" />
+        <input onChange={onChange} type="text" name="title" required id="title" value={title} />
       </div>
       <div className="form__block">
         <label htmlFor="summary">요약</label>
-        <input onChange={onChange} type="text" name="summary" required id="summary" />
+        <input
+          onChange={onChange}
+          type="text"
+          name="summary"
+          required
+          id="summary"
+          value={summary}
+        />
       </div>
       <div className="form__block">
         <label htmlFor="content">내용</label>
-        <textarea onChange={onChange} name="content" id="content" required />
+        <textarea onChange={onChange} name="content" id="content" required value={content} />
       </div>
       <div className="form__block">
         <input type="submit" value="제출" className="form__btn-submit" />

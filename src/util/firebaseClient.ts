e2735e5eed8bd2001e60333ee9c2d5a8a05d4359
SeungGukYoo <dbtmd878@gmail.com>
2023-11-06 15:00:51 +0900

    feat: 데이터 호출 삭제 기능 추가

--- src/util/firebaseClient.ts
+++ src/util/firebaseClient.ts
@@ -1,15 +1,34 @@
-import { DocumentData, doc, getDoc } from 'firebase/firestore';
+import {
+  DocumentData,
+  QuerySnapshot,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+} from 'firebase/firestore';
 import { db } from 'firebaseApp';
 
 interface IFirebase {
-  getPosts: (id: string) => Promise<DocumentData | undefined>;
+  getPosts: (id: string) => Promise<QuerySnapshot<DocumentData, DocumentData>>;
+  getPost: (id: string) => Promise<DocumentData | undefined>;
+  deleteData: (id: string) => Promise<void>;
 }
 
 export class FirebaseClient implements IFirebase {
   constructor() {}
-  async getPosts(id: string) {
+  async getPosts() {
+    const docSnap = await getDocs(collection(db, 'posts'));
+    return docSnap;
+  }
+
+  async getPost(id: string) {
     const docRef = doc(db, 'posts', id);
     const docSnap = await getDoc(docRef);
-    return docSnap.data();
+    return docSnap;
+  }
+
+  async deleteData(id: string) {
+    return await deleteDoc(doc(db, 'posts', id));
   }
 }

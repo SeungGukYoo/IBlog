e3d135065f5d11fb6326839740529361c0035f1e
SeungGukYoo <dbtmd878@gmail.com>
2023-11-07 00:21:42 +0900

    feat: 전역 테마 클래스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Loader from 'components/Loader';
 import Router from 'components/Router';
 import AuthContext from 'context/AuthContext';
+import ThemeContext from 'context/ThemeContext';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import app from 'firebaseApp';
 import React, { useContext, useEffect, useState } from 'react';
@@ -8,6 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const { theme } = useContext(ThemeContext);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!getAuth(app)?.currentUser);
   const [init, setInit] = useState(false);
   const { auth } = useContext(AuthContext);
@@ -23,10 +25,10 @@ function App() {
     });
   }, [auth]);
   return (
-    <>
+    <div className={theme === 'light' ? 'white' : 'dark'}>
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
       <ToastContainer />
-    </>
+    </div>
   );
 }
 

abb7a25146d132b72867b05ab54f2625f25267a5
SeungGukYoo <dbtmd878@gmail.com>
2023-11-07 00:21:20 +0900

    feat: 테마 토글 기능 구현

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -7,8 +7,12 @@ const ThemeContext = createContext({
 });
 
 export function ThemeContextProvider({ children }: ContextProps) {
-  const [theme, setTheme] = useState('light');
-  const toggleMode = () => setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+  const [theme, setTheme] = useState(window.localStorage.getItem('theme') || 'light');
+  const toggleMode = () => {
+    const changeTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(changeTheme);
+    localStorage.setItem('theme', changeTheme);
+  };
 
   return <ThemeContext.Provider value={{ theme, toggleMode }}>{children}</ThemeContext.Provider>;
 }
